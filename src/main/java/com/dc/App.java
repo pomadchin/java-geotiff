08b882c96ef89ff4da67e21f09ab56509afb4771
Grisha Pomadchin <gr.pomadchin@gmail.com>
2014-11-10 02:27:20 +0400

    init commit

---
+++ src/main/java/com/dc/App.java
@@ -0,0 +1,15 @@
+package com.dc;
+
+import com.dc.core.GeoTIFF;
+
+public class App  {
+
+    public static void main(String[] args) {
+        GeoTIFF tiff = new GeoTIFF();
+        tiff.imageIO(false);
+        /*System.out.println(new Date());
+        tiff.merge();
+        System.out.println(new Date());*/
+    }
+
+}
---
+++ src/main/java/com/dc/core/GeoTIFF.java
@@ -0,0 +1,152 @@
+package com.dc.core;
+
+
+import com.sun.media.jai.codec.ImageCodec;
+import com.sun.media.jai.codec.ImageEncoder;
+import com.sun.media.jai.codec.TIFFEncodeParam;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import javax.media.jai.PlanarImage;
+import javax.media.jai.RenderedImageAdapter;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class GeoTIFF {
+    private String path = "/mnt/disk2/subversions/git/github/geotrellis-spray-tutorial/data/geotiff/";
+    private String filepath = "";
+    private PlanarImage[] images = new PlanarImage[3];
+    private OutputStream os = null;
+    private RenderedImage renderedImage;
+
+    public GeoTIFF() {
+        try {
+            renderedImage = javax.imageio.ImageIO.read(new File(path + "LC81770282014145LGN00.TIF"));
+            images[0] = (PlanarImage) new RenderedImageAdapter(renderedImage);
+
+            renderedImage = javax.imageio.ImageIO.read(new File(path + "LC81770282014209LGN00.TIF"));
+            images[1] = (PlanarImage) new RenderedImageAdapter(renderedImage);
+
+            renderedImage = javax.imageio.ImageIO.read(new File(path + "LC81770282014241LGN00.TIF"));
+            images[2] = (PlanarImage) new RenderedImageAdapter(renderedImage);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void imageIO(Boolean f) {
+        try {
+            System.out.println(new Date());
+            saveTiffImageIO(images);
+            if(f) saveTiffPng();
+            System.out.println(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void JAI(Boolean f) {
+        try {
+            System.out.println(new Date());
+            saveTiffJAI(os, images);
+            if(f) saveTiffPng();
+            System.out.println(new Date());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveTiffImageIO(PlanarImage[] images)
+            throws IOException {
+        Iterator<ImageWriter> writers = ImageIO
+                .getImageWritersByFormatName("tiff");
+        ImageWriter imageWriter = writers.next();
+
+        ImageOutputStream ios = ImageIO.createImageOutputStream(new File(path + "result.TIF"));
+        imageWriter.setOutput(ios);
+
+        imageWriter.prepareWriteSequence(null);
+        for (PlanarImage image : images) {
+            imageWriter.writeToSequence(new IIOImage(image, null, null), null);
+        }
+        imageWriter.endWriteSequence();
+
+        imageWriter.dispose();
+        ios.flush();
+        ios.close();
+    }
+
+    private void saveTiffPng() throws IOException {
+        final BufferedImage tif = ImageIO.read(new File(path + "result.TIF"));
+        ImageIO.write(tif, "png", new File(path + "result.png"));
+    }
+
+    private void saveTiffJAI(OutputStream out, PlanarImage[] images)
+            throws IOException {
+        TIFFEncodeParam param = new TIFFEncodeParam();
+        ImageEncoder encoder = ImageCodec
+                .createImageEncoder("TIFF", out, param);
+        List<PlanarImage> list = new ArrayList<PlanarImage>();
+        for (int i = 1; i < images.length; i++) {
+            list.add(images[i]);
+        }
+        param.setExtraImages(list.iterator());
+        encoder.encode(images[0]);
+    }
+
+    /*public void merge() {
+        try {
+            String targetDir = path + "result2.tiff";
+            BufferedImage img2 = new BufferedImage(images[0].getWidth(), images[0].getHeight(),
+                    BufferedImage.TYPE_INT_RGB);
+
+            BufferedImage image0 = ImageIO.read(new File(path + "LC81770282014145LGN00.TIF"));
+            BufferedImage image1 = ImageIO.read(new File(path + "LC81770282014209LGN00.TIF"));
+            BufferedImage image2 = ImageIO.read(new File(path + "LC81770282014241LGN00.TIF"));
+
+            for(int y = 0; y < images[0].getHeight(); y++) {
+                for(int x = 0; x < images[0].getWidth(); x++) {
+                    img2.setRGB(x, y, (image0.getRGB(x, y) + image1.getRGB(x, y) + image.getRGB(x, y)) / 3);
+                }
+            }
+            File outputFile = new File(targetDir);
+            outputFile.getParentFile().mkdirs();
+            outputFile.createNewFile();
+            ImageIO.write(img2, "tiff", outputFile);
+        } catch (Exception e) { e.printStackTrace(); }
+    }*/
+
+    public void toPng() {
+        try {
+            File file = new File(path + "result.TIF");
+            String targetDir = path + "result2.png";
+            BufferedImage image = ImageIO.read(file);
+            if(image != null) {
+                BufferedImage img2 = new BufferedImage(image.getWidth(), image.getHeight(),
+                        BufferedImage.TYPE_INT_RGB);
+                for(int y = 0; y < image.getHeight(); y++) {
+                    for(int x = 0; x < image.getWidth(); x++) {
+                        img2.setRGB(x, y, image.getRGB(x, y));
+                    }
+                }
+                File outputFile = new File(targetDir);
+                outputFile.getParentFile().mkdirs();
+                outputFile.createNewFile();
+                ImageIO.write(img2, "png", outputFile);
+            } else {
+                System.out.println("image is null for file " + file.getAbsolutePath());
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+
+}
